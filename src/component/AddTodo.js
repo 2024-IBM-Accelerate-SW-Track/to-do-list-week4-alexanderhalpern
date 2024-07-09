f329552cf640b6331fc8ce21143faaedcc869ff6
alexanderhalpern <halperna22@gmail.com>
2024-07-08 21:57:54 -0400

    working!

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,3 +1,4 @@
+import Axios from "axios";
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
 import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
@@ -39,6 +40,22 @@ class AddTodo extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
+      const jsonObject = {
+        id: this.state.id,
+        task: this.state.content,
+        currentDate: this.state.date,
+        dueDate: this.state.duedate
+     };
+     Axios({
+      method: "POST",
+      url: "http://localhost:3001/add/item",
+      data: {jsonObject},
+      headers: {
+         "Content-Type": "application/json"
+      }
+   }).then(res => {
+      console.log(res.data.message);
+   });
       this.props.addTodo(this.state);
       this.setState({
         content: "",
